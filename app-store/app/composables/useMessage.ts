0ccfa2d3a645d1d7d3621d976e74644eb2c2c6a5
limvichet <83205736+limvichet@users.noreply.github.com>
2026-04-17 19:18:25 +0700

    fix index.vue

--- app-store/app/composables/useMessage.ts
+++
@@ -1,8 +0,0 @@
-import { ref } from "vue";
-
-const successMsg = ref<string | null>(null)
-const errorMsg = ref<string | null>(null)
-
-export const useMessage = () => {
-  return { successMsg, errorMsg }
-}
